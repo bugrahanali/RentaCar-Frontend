e3f4a809cb496e3c15fc5dae28ec3441e3cf9870
bugrahanali <bugra.ali.06@gmail.com>
2021-04-24 18:12:33 +0300

    18.gün 2.commit

--- src/app/components/brand/brand.component.ts
+++ src/app/components/brand/brand.component.ts
@@ -10,6 +10,7 @@ import { BrandService } from 'src/app/services/brand.service';
 export class BrandComponent implements OnInit {
 
   brands:Brand[]=[]
+  currentBrand : Brand;
   dataLoaded=false;
 
 
@@ -25,6 +26,17 @@ export class BrandComponent implements OnInit {
       this.brands=response.data;
       this.dataLoaded=true;
       });
+  }
+  setCurrentBrand(brand:Brand ){
+    
+    this.currentBrand=brand;
+  }
+  getCurrentBrand(brand:Brand){
+    if (this.currentBrand === brand)  {
+      return "list-group-item active";
+    } else {
+      return "list-group-item"
+    }
   }
 
 }
